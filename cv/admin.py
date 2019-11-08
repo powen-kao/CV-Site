4ab86ff73f3db23fc478d4c123a879c7fbe1c853
Powen Kao <hp5588@gmail.com>
2019-11-08 20:56:12 +0100

    - add DB - update awesome-font - model update - page content update

--- cv/admin.py
+++ cv/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
-from .models import Experience
+from .models import Experience, Education, Skill
 
 # Register your models here.
-admin.site.register(Experience)
+# admin.site.register(Experience)
+# admin.site.register(Education)
+# admin.site.register(Skill)
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'position', 'department', 'start_date', 'end_date')
+    save_as = True
+
+
+@admin.register(Education)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('university', 'major', 'start_date', 'end_date')
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level')
--- cv/models.py
+++ cv/models.py
@@ -4,8 +4,20 @@ from django.db import models
 # Create your models here.
 class Experience(models.Model):
     title = models.CharField(max_length=100)
+    department = models.CharField(max_length=100, blank=True)
+    position = models.CharField(max_length=50)
     description = models.TextField()
     start_date = models.DateField('start date')
     end_date = models.DateField('end date')
 
 
+class Education(models.Model):
+    university = models.CharField(max_length=100)
+    major = models.CharField(max_length=100)
+    start_date = models.DateField('start date')
+    end_date = models.DateField('end date')
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    level = models.IntegerField('level of skill range from 0 to 10')
