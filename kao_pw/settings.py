6c2134f9816a3d21d285dc2709d40f75a8761866
Po-Wen Kao <hp5588@gmail.com>
2020-06-29 20:38:49 +0200

    configurable db key

--- kao_pw/settings.py
+++ kao_pw/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!ytas#p)aip)w=my*!dk*vug=&!6(7vfckjaa)xw_1^_m#&fk4'
+SECRET_KEY = os.getenv("ENV_DB_KEY", "setupdbkeyhere")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("ENV_DEBUG", "True") == "True"
